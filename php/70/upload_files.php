0189a05900015393060688333060b9140b15529d
Mandar Bhaskar <141809770+mandarbhaskar@users.noreply.github.com>
2025-04-30 13:05:58 +0530

    Add files via upload

---
+++ php/70/upload_files.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        Select file to upload:
+        <input type="file" name="fileToUpload" required> </br>
+        <input type="submit" value="Upload File">
+    </form>
+</body>
+</html>
